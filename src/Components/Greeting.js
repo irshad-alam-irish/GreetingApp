488dfac7a4803e1f025c9c9ada487875578375f5
irshad-alam-irish <112090447+irshad-alam-irish@users.noreply.github.com>
2023-10-31 16:53:24 +0530

    redirect file added

--- src/Components/Greeting.js
+++ src/Components/Greeting.js
@@ -4,12 +4,13 @@ import './Greeting.css'
 function Greeting(props) {
   return (
     <div id="greetings">
-        <div className='div11'>
-           <div className='greet1'><h1>Hello <span className='name1'>{props.name},</span> yayy <span className='congo1'>Congratulations</span>, you are about to join, by mail: {props.email}, <span className='irish1'>Irish</span> will be considring your application Soon!</h1></div>
+      <div className='div11'>
+        <div className='greet1'><h1>Hello <span className='name1'>{props.name},</span> yayy <span className='congo1'>Congratulations</span>, you are about to join, by mail: {props.email}, <span className='irish1'>Irish</span> will be considring your application Soon!</h1>
         </div>
-        <div className='div22'>
-            <img id="bachha" src={require('./pics/bacha.png')} alt="" />
-        </div>
+      </div>
+      <div className='div22'>
+        <img id="bachha" src={require('./pics/bacha.png')} alt="" />
+      </div>
     </div>
   )
 }
